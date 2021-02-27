9345aac3f126297da63ab3b3f317891a50a6e616
Matthew Cain <yooksi@zoho.com>
2021-02-27 19:32:34 +0100

    Stop cleaning before assembling dist

--- distribution.sh
+++ distribution.sh
@@ -2,7 +2,7 @@
 echo Listing files ignored by git...
 find . -path ./.git -prune -o -print | git check-ignore --no-index --stdin > .gitignored
 
-echo Cleaning and assembling distribution...
-./gradlew clean assembleDist
+echo Assembling distribution...
+./gradlew assembleDist
 
 read -p "Press enter to continue"
