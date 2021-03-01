408b9d86f1070874d9c0ea60453685ce02038bb9
Matthew Cain <yooksi@zoho.com>
2021-03-01 22:26:49 +0100

    Fix distribution including ignored files
    The exclusion system for staging distribution files did not work properly.

--- distribution.sh
+++ distribution.sh
@@ -1,8 +1,26 @@
 #!/bin/bash
-echo Listing files ignored by git...
+
+printf 'Staging distribution...\n'
+./gradlew stageDistribution
+
+printf '\nListing files ignored by git...\n'
+
+# disable distribution gitignore
+mv stage/.gitignore stage/.gitignore_
+
 find . -path ./.git -prune -o -print | git check-ignore --no-index --stdin > .gitignored
 
-echo Assembling distribution...
+# enable distribution gitignore
+mv stage/.gitignore_ stage/.gitignore
+
+printf '\nPruning distribution stage...\n'
+./gradlew pruneDistributionStage
+
+printf '\nAssembling distribution...\n'
 ./gradlew assembleDist
 
+printf '\nCleaning distribution stage...\n'
+./gradlew cleanDistributionStage
+
+printf '\n'
 read -p "Press enter to continue"
